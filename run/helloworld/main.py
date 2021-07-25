79b385a2bcf193734b5ce25495ef4327d59c6b34
GoogleCloudPlatform <34994978+shivpj@users.noreply.github.com>
2021-07-25 21:16:09 +0530

    Update main.py

--- run/helloworld/main.py
+++ run/helloworld/main.py
@@ -1,9 +1,15 @@
 import os
 
-from flask import Flask
+from firebase_admin import credentials, firestore, initialize_app
 
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('users')
 
 @app.route("/")
 def hello_world():
@@ -15,7 +21,27 @@ def PJ1():
     name = os.environ.get("NAME", "PJ")
     return "I am {}!".format(name)
 
+@app.route('/list', methods=['GET'])
+def read():
+    """
+        read() : Fetches documents from Firestore collection as JSON
+        todo : Return document that matches query ID
+        all_todos : Return all documents
 
+    """
+    try:
+        # Check if ID was passed to URL query
+        todo_id = request.args.get('id')    
+        if todo_id:
+            todo = todo_ref.document(todo_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            return jsonify(all_todos), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 # [END run_helloworld_service]
